61e11d3d6c32b0115b1de3b5ca0ef9ad38c8796c
emreceran <emrecrn@gmail.com>
2025-08-07 20:08:41 +0300

    asa

--- models/sale_order.py
+++ models/sale_order.py
@@ -60,3 +60,50 @@ class SaleOrder(models.Model):
 
             # Yeni para birimini kaydediyoruz
             order.previous_currency_id = new_currency
+
+    # def action_generate_rfq_by_need(self):
+    #     for order in self:
+    #         supplier_po_map = {}
+    #
+    #         for line in order.order_line:
+    #             if line.product_id.type != 'product':
+    #                 continue  # Stoklanabilir olmayan ürünler pas geçilir
+    #
+    #             needed_qty = line.product_uom_qty - line.free_qty_today
+    #             if needed_qty <= 0:
+    #                 continue  # Stok yeterliyse RFQ'ya gerek yok
+    #
+    #             supplierinfo = line.product_id._select_seller(
+    #                 quantity=needed_qty,
+    #                 uom_id=line.product_uom,
+    #                 date=fields.Date.today()
+    #             )
+    #
+    #             partner = supplierinfo.partner_id
+    #             purchase_order = supplier_po_map.get(partner.id)
+    #             if not purchase_order:
+    #                 purchase_order = self.env['purchase.order'].search([
+    #                     ('partner_id', '=', partner.id),
+    #                     ('state', '=', 'draft'),
+    #                     ('company_id', '=', line.company_id.id),
+    #                 ], limit=1)
+    #                 if not purchase_order:
+    #                     purchase_order = self.env['purchase.order'].create({
+    #                         'partner_id': partner.id,
+    #                         'date_order': fields.Date.today(),
+    #                         'origin': order.name,
+    #                         'company_id': line.company_id.id,
+    #                     })
+    #                 supplier_po_map[partner.id] = purchase_order
+    #
+    #             purchase_qty = line.product_uom._compute_quantity(needed_qty, line.product_id.uom_po_id)
+    #             self.env['purchase.order.line'].create({
+    #                 'order_id': purchase_order.id,
+    #                 'product_id': line.product_id.id,
+    #                 'product_qty': purchase_qty,
+    #                 'product_uom': line.product_id.uom_po_id.id,
+    #                 'price_unit': supplierinfo.price,
+    #                 'date_planned': fields.Date.today(),
+    #                 'name': line.name,
+    #                 'sale_line_id': line.id,
+    #             })
